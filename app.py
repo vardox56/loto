20e24b1294d6a19a399c3dfbc80a5a1348a125d6
vardox56 <allanchellat@gmail.com>
2025-12-27 23:25:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -463,4 +463,7 @@ if __name__ == '__main__':
     os.makedirs('static', exist_ok=True)
     
     # Lancer le serveur
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
+
